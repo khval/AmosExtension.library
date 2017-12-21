3754e19b9cdfe302427b94ce772278a688a4ad13
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-12-21 17:31:40 +0100

    List all extension commands, just a example, to show how simple it is to list all commands.
    List all extension commands, just a example, to show how simple it is to
    list all commands.

---
+++ amosextension_files/tests/list.cpp
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <proto/exec.h>
+#include <proto/AmosExtension.h>
+#include <string.h>
+#include "init.h"
+
+int main()
+{
+	struct extension *ext = NULL;
+	TokenInfo *info;
+	char *name;
+	struct ExtensionDescriptor *ed;
+
+	if (init())
+	{
+		ext = OpenExtension( (char *) "amospro:APSystem/AMOSPro_Request.lib");
+
+		if (ext)
+		{
+			for ( ed =FirstExtensionItem( ext ); ed ; ed = NextExtensionItem( ed ))
+			{
+				printf("%04X,%04x,%d,%d\n", 
+							ed -> fileOffset,
+							ed -> tokenInfo.token,
+							ed -> tokenInfo.NumberOfInstruction,
+							ed -> tokenInfo.NumberOfFunction);
+
+				printf("%s: %s\n", 
+						(ed -> tokenInfo.command) ? ed -> tokenInfo.command : "NULL",
+						(ed -> tokenInfo.args) ? ed -> tokenInfo.args : "NULL"
+					);
+			}
+
+			CloseExtension(ext);
+		}
+	}
+
+	closedown( );
+
+	return 0;
+}
+
